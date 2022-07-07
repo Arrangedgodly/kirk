597688ea09def3a1731c999b8278628675e5e23c
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-07-06 23:01:41 -0600

    refactor: reworked site to add cards via JS

---
+++ scripts/card.js
@@ -0,0 +1,32 @@
+const cardTemplate = document.querySelector("#card").content;
+const cards = document.querySelector(".cards");
+
+function createCard(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardElementImage = cardElement.querySelector(".card__image");
+  const cardElementHeader = cardElement.querySelector(".card__title");
+  const cardElementRelease = cardElement.querySelector(".card__desc");
+  const cardElementSoundcloud = cardElement.querySelector(".card__link_soundcloud");
+  const cardElementSpotify = cardElement.querySelector(".card__link_spotify");
+  const cardElementYoutube = cardElement.querySelector(".card__link_youtube");
+  const cardTitle = data.name;
+  const cardImage = data.image;
+  const cardRelease = data.release;
+  const cardSoundcloud = data.soundcloud;
+  const cardSpotify = data.spotify;
+  const cardYoutube = data.youtube;
+  cardElementHeader.textContent = cardTitle;
+  cardElementRelease.textContent = `Released: ${ cardRelease}`;
+  cardElementSoundcloud.setAttribute("src", cardSoundcloud);
+  cardElementSpotify.setAttribute("src", cardSpotify);
+  cardElementYoutube.setAttribute("src", cardYoutube);
+  cardElementImage.setAttribute("src", cardImage);
+  cardElementImage.setAttribute("alt",`Single Cover for ${cardTitle}`);
+  return cardElement;
+}
+
+function renderCard(card) {
+  cards.prepend(card);
+}
+
+initialCards.forEach(element => renderCard(createCard(element)));
---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+let initialCards = [
+  {
+    name: "SOME DAZE",
+    release: "January 1st, 2022",
+    soundcloud: "https://soundcloud.com/maytrax/drip-drip",
+    spotify: "https://open.spotify.com/track/3dpR0s4GOitveeIS0Lms5v?si=7ed8973408664d37",
+    youtube: "https://youtu.be/D5CDTO14po0",
+    image: "./images/somedaze.jpeg"
+  },
+  {
+    name: "DRiP DRiP",
+    release: "March 11th, 2022",
+    soundcloud: "https://soundcloud.com/maytrax/rager-in-the-grave-freestyle",
+    spotify: "https://open.spotify.com/track/1uiIMo9hS3W133kN8vpPgP?si=f1899536c8974a63",
+    youtube: "https://www.youtube.com/watch?v=s19Bb9KZ_OM",
+    image: "./images/dripdrip.png"
+  },
+  {
+    name: "RAGER IN THE GRAVE",
+    release: "June 5th, 2022",
+    soundcloud: "https://soundcloud.com/maytrax/rager-in-the-grave-freestyle",
+    spotify: "https://open.spotify.com/track/1uiIMo9hS3W133kN8vpPgP?si=f1899536c8974a63",
+    youtube: "https://www.youtube.com/watch?v=UAtdOHf8Vog",
+    image: "./images/rager.png"
+  },
+  {
+    name: "SLVR SURFER",
+    release: "July 6th, 2022",
+    soundcloud: "https://soundcloud.com/maytrax",
+    spotify: "https://open.spotify.com/album/7AgdpR3ievOolUcHsOzHdg?si=XxkxLPAwQI-mX9PippWCtA",
+    youtube: "https://youtu.be/A4Ks_XceN24",
+    image: "./images/slvr.png"
+  }
+  /*
+  {
+    name:
+    release:
+    soundcloud:
+    spotify:
+    youtube:
+    image:
+  },
+  */
+]
